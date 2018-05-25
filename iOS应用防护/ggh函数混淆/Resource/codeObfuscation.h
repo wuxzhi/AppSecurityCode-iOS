1fc86a0ce45d1a2c40a585c11314372b25c1b86a
wxzhi <wxzhi_uian@163.com>
2018-05-25 18:13:53 +0800

    简单安全防护
    简单安全防护

---
+++ iOS应用防护/ggh函数混淆/Resource/codeObfuscation.h
@@ -0,0 +1,8 @@
+#ifndef Demo_codeObfuscation_h
+#define Demo_codeObfuscation_h
+//confuse string at Fri May 25 17:52:53 CST 2018
+#define ggh_func1 gSHEfGZWxvUoUtbj
+#define ggh_func2 QLcrwUQnSfSKKTJx
+#define ggh_func3 oXkSDwIBdPKoeIbZ
+#define ggh_func4 OUVVZWhPjoeeEogL
+#endif
---
+++ iOS应用防护/ggh函数混淆/ggh函数混淆/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ggh函数混淆
+//
+//  Created by guogh on 2017/7/16.
+//  Copyright © 2017年 wxzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
